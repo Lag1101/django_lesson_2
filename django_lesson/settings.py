897fd0ac61607a9deec9f2e95386456a0fccca02
luckybug <lomanovvasiliy@gmail.com>
2020-02-24 12:21:34 +0300

    using DATABASE_URL

--- django_lesson/settings.py
+++ django_lesson/settings.py
@@ -82,11 +82,7 @@ WSGI_APPLICATION = 'django_lesson.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
+        'HOST': os.environ.get('DATABASE_URL'),
     }
 }
 
